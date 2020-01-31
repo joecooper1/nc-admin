724c9e442ba7bd42660a6bea837794a2090e7d27
CallumGoodley <callumgoodley@btinternet.com>
2020-01-31 11:45:40 +0000

    all functionality for students complete

--- src/Components/StudentList.jsx
+++ src/Components/StudentList.jsx
@@ -1,30 +1,25 @@
-import React from "react";
+import React from 'react';
 
-const StudentList = ({ student_list, selectStudent, addToGraduating }) => {
-  return (
-    <main>
-      <h1>List of students:</h1>
-      <form>
-        {student_list.map(student => {
-          return (
-            <label key={student._id}>
-              <button onClick={selectStudent} name={student._id}>
-                {student.name}, cohort:{student.startingCohort}, block:
-                {student.currentBlock}
-              </button>
-              <input
-                type="checkbox"
-                value={student._id}
-                name={student.name}
-                onChange={addToGraduating}
-              />
-            </label>
-          );
-        })}
-      </form>
-      <button>Graduate selected</button>
-    </main>
-  );
+const StudentList = ({ student_list, selectStudent, addToGraduating, graduateStudents }) => {
+	return (
+		<main>
+			<h1>List of students:</h1>
+			<form onSubmit={graduateStudents}>
+				{student_list.map((student) => {
+					return (
+						<label key={student._id}>
+							<button onClick={selectStudent} name={student._id}>
+								{student.name}, cohort:{student.startingCohort}, block:
+								{student.currentBlock}
+							</button>
+							<input type="checkbox" value={student._id} name={student.name} onChange={addToGraduating} />
+						</label>
+					);
+				})}
+				<button>Graduate selected</button>
+			</form>
+		</main>
+	);
 };
 
 export default StudentList;
--- src/Components/Students.jsx
+++ src/Components/Students.jsx
@@ -1,105 +1,117 @@
-import React from "react";
-import StudentList from "./StudentList";
-import Student from "./Student";
-import SearchBar from "./SearchBar";
-import axios from "axios";
-import AddStudent from "./AddStudent";
+import React from 'react';
+import StudentList from './StudentList';
+import Student from './Student';
+import SearchBar from './SearchBar';
+import axios from 'axios';
+import AddStudent from './AddStudent';
 
 class Students extends React.Component {
-  state = {
-    student_list: [],
-    display_student: {
-      name: "",
-      startingCohort: "",
-      blockHistory: [{ name: "" }]
-    },
-    graduating_students: []
-  };
+	state = {
+		student_list: [],
+		display_student: {
+			name: '',
+			startingCohort: '',
+			blockHistory: [ { name: '' } ]
+		},
+		graduating_students: [],
+		just_graduated: []
+	};
 
-  render() {
-    return (
-      <main>
-        <SearchBar searchResults={this.searchResults} />
-        <AddStudent addNewStudent={this.addNewStudent} />
-        <Student display_student={this.state.display_student} />
-        <StudentList
-          student_list={this.state.student_list}
-          selectStudent={this.selectStudent}
-          addToGraduating={this.addToGraduating}
-        />
-      </main>
-    );
-  }
+	render() {
+		return (
+			<main>
+				<SearchBar searchResults={this.searchResults} />
+				<AddStudent addNewStudent={this.addNewStudent} />
+				<Student display_student={this.state.display_student} />
+				<StudentList
+					student_list={this.state.student_list}
+					selectStudent={this.selectStudent}
+					addToGraduating={this.addToGraduating}
+					graduateStudents={this.graduateStudents}
+				/>
+			</main>
+		);
+	}
+	componentDidUpdate(prevProps, prevState) {
+		if (this.state.just_graduated.length > prevState.just_graduated.length)
+			axios.get('https://nc-student-tracker.herokuapp.com/api/students?sort_by=name').then((res) => {
+				this.setState({ student_list: res.data.students });
+			});
+	}
 
-  componentDidMount() {
-    axios
-      .get("https://nc-student-tracker.herokuapp.com/api/students?sort_by=name")
-      .then(res => {
-        this.setState({ student_list: res.data.students });
-      });
-  }
+	componentDidMount() {
+		axios.get('https://nc-student-tracker.herokuapp.com/api/students?sort_by=name').then((res) => {
+			this.setState({ student_list: res.data.students });
+		});
+	}
 
-  searchResults = ({ search_input, block_input, cohort_input }) => {
-    if (block_input === "Block") block_input = "";
-    if (cohort_input === "Cohort") cohort_input = "";
-    axios
-      .get(
-        `https://nc-student-tracker.herokuapp.com/api/students?block=${block_input}&cohort=${cohort_input}&sort_by=name&order=asc`
-      )
-      .then(res => {
-        console.log("submit");
-        this.setState({ student_list: res.data.students });
-      })
-      .catch(() => {
-        console.log("error");
-      });
-  };
+	searchResults = ({ search_input, block_input, cohort_input }) => {
+		if (block_input === 'Block') block_input = '';
+		if (cohort_input === 'Cohort') cohort_input = '';
+		axios
+			.get(
+				`https://nc-student-tracker.herokuapp.com/api/students?block=${block_input}&cohort=${cohort_input}&sort_by=name&order=asc`
+			)
+			.then((res) => {
+				this.setState({ student_list: res.data.students });
+			})
+			.catch(() => {
+				console.log('error');
+			});
+	};
 
-  addNewStudent = ({ new_student_input, new_student_cohort_input }) => {
-    axios
-      .post("https://nc-student-tracker.herokuapp.com/api/students", {
-        name: new_student_input,
-        startingCohort: new_student_cohort_input
-      })
-      .then(res => {
-        this.setState(currentState => {
-          return {
-            student_list: [...currentState.student_list, res.data.student]
-          };
-        });
-      });
-  };
+	addNewStudent = ({ new_student_input, new_student_cohort_input }) => {
+		axios
+			.post('https://nc-student-tracker.herokuapp.com/api/students', {
+				name: new_student_input,
+				startingCohort: new_student_cohort_input
+			})
+			.then((res) => {
+				this.setState((currentState) => {
+					return {
+						student_list: [ ...currentState.student_list, res.data.student ]
+					};
+				});
+			});
+	};
 
-  selectStudent = event => {
-    event.preventDefault();
-    const { name } = event.target;
-    axios
-      .get(`https://nc-student-tracker.herokuapp.com/api/students/${name}`)
-      .then(res => {
-        console.log(res.data.student);
-        this.setState({ display_student: res.data.student });
-      });
-  };
+	selectStudent = (event) => {
+		event.preventDefault();
+		const { name } = event.target;
+		axios.get(`https://nc-student-tracker.herokuapp.com/api/students/${name}`).then((res) => {
+			this.setState({ display_student: res.data.student });
+		});
+	};
 
-  addToGraduating = event => {
-    const newStudent = event.target.value;
-    this.setState(currentState => {
-      if (
-        currentState.graduating_students.filter(
-          student => student === newStudent
-        ).length === 0
-      ) {
-        return {
-          graduating_students: [...currentState.graduating_students, newStudent]
-        };
-      } else
-        return {
-          graduating_students: currentState.graduating_students.filter(
-            student => student !== newStudent
-          )
-        };
-    });
-  };
+	graduateStudents = (event) => {
+		event.preventDefault();
+		this.state.graduating_students.map((student) =>
+			axios
+				.patch(`https://nc-student-tracker.herokuapp.com/api/students/${student}?progress=true`)
+				.then((res) => {
+					this.setState((currentState) => {
+						return { just_graduated: [ ...currentState.just_graduated, res.data.student ] };
+					});
+					this.setState({ graduating_students: [] });
+					document.querySelectorAll('input[type=checkbox]').forEach((el) => (el.checked = false));
+				})
+		);
+	};
+
+	addToGraduating = (event) => {
+		const newStudent = event.target.value;
+		const check = event.target.checked;
+		this.setState((currentState) => {
+			if (check === true) {
+				return {
+					graduating_students: [ ...currentState.graduating_students, newStudent ]
+				};
+			} else
+				return {
+					graduating_students: currentState.graduating_students.filter((student) => student !== newStudent)
+				};
+		});
+	};
 }
 
 export default Students;
